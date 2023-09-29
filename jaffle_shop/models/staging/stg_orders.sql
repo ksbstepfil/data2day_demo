e4adeb544fb6aca1093fa660a181b2e9c5a186be
Filip <filip.stepniak@esentri.com>
2023-09-29 22:13:50 +0200

    upstream raw data

--- jaffle_shop/models/staging/stg_orders.sql
+++ jaffle_shop/models/staging/stg_orders.sql
@@ -3,9 +3,10 @@ with source as (
     {#-
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
+    -- select * from {{ ref('raw_orders') }}
     #}
-    select * from {{ ref('raw_orders') }}
 
+    select * from {{ source('PUBLIC', 'raw_orders') }}
 ),
 
 renamed as (
--- jaffle_shop/models/staging/stg_payments.sql
+++ jaffle_shop/models/staging/stg_payments.sql
@@ -3,8 +3,9 @@ with source as (
     {#-
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
+    -- select * from {{ ref('raw_payments') }}
     #}
-    select * from {{ ref('raw_payments') }}
+    select * from {{ source('PUBLIC', 'raw_payments') }}
 
 ),
 
